70e879a61ca74e3b969f43927424a3995562f8b0
gor2em <grkem.squash99@gmail.com>
2022-02-15 13:27:15 +0300

    Post model added hasMany

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function category()
+    {
+        //one to one = hasOne
+        //one to many = hasMany
+        return $this->hasMany(Category::class, 'id', 'category_id');
+    }
 }
